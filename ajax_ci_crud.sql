56ff95fa87043d4d32beb94deb5304ce1c543b39
Dinesh99v99 <Dineshdin7010@gmail.com>
2021-07-25 10:46:09 +0530

    sql insert.

--- ajax_ci_crud.sql
+++ ajax_ci_crud.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
--- http://www.phpmyadmin.net
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2015 at 08:34 PM
--- Server version: 5.6.16
--- PHP Version: 5.5.11
+-- Generation Time: Jul 25, 2021 at 07:12 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `ajax_ci_crud`
@@ -26,25 +27,47 @@ SET time_zone = "+00:00";
 -- Table structure for table `persons`
 --
 
-CREATE TABLE IF NOT EXISTS `persons` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `persons` (
+  `id` int(11) UNSIGNED NOT NULL,
   `firstName` varchar(100) DEFAULT NULL,
   `lastName` varchar(100) DEFAULT NULL,
-  `gender` enum('male','female') DEFAULT NULL,
-  `address` varchar(200) DEFAULT NULL,
-  `dob` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+  `pic_id` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `persons`
 --
 
-INSERT INTO `persons` (`id`, `firstName`, `lastName`, `gender`, `address`, `dob`) VALUES
-(1, 'Ehtesham', 'Mehmood', 'male', 'Dar mill street sargodha road gujrat', '1993-02-17'),
-(2, 'Sharjeel', 'Mukhtar', 'male', 'Gujrat', '2015-12-08'),
-(3, 'moazam', 'moazam', 'male', 'Pakistan', '2004-09-22'),
-(4, 'Mutafaf', 'wahaj', 'male', 'Pakistan , punjab', '2004-11-03');
+INSERT INTO `persons` (`id`, `firstName`, `lastName`, `pic_id`) VALUES
+(2, 'Sharjeel', 'Mukhtar', NULL),
+(6, 'Dinesh', 'v', NULL),
+(7, 'varun', ' B', NULL),
+(8, 'Dinesh', 'v', NULL),
+(9, 'Dinesh', 'v', NULL),
+(10, 'Dinesh', 'v', NULL),
+(11, 'Dinesh', 'v', NULL),
+(12, 'Dinesh', 'v', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `persons`
+--
+ALTER TABLE `persons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `persons`
+--
+ALTER TABLE `persons`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
